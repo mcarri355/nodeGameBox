d0d0fe93e35c7dbb447837e0232267c443f138bf
mcarri355 <carrillomatisa@gmail.com>
2023-10-19 13:27:42 -0700

    push

--- models/user.js
+++ models/user.js
@@ -1,26 +1,29 @@
 const mongoose = require('mongoose');
-const UserSchema  = new mongoose.Schema({
-    first_name :{
-        type  : String,
-        required : true
-    } ,
-    last_name :{
-        type  : String,
-        required : true
-    } ,
-    email :{
-    type  : String,
-    required : true
-    } ,
-    password :{
-        type  : String,
-        required : true
-    } ,
-    date :{
-        type : Date,
-        default : Date.now
-    }
-},{collection : 'MemoryUsers'});
-const User= mongoose.model('User',UserSchema);
+const UserSchema = new mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { collection: 'MemoryUsers' }
+);
+const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
